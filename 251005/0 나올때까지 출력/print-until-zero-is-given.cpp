8176d8c0cb03f931ce2b7b518e1f49d73ae0148d
Minsu Kim <kimminsu31415@gmail.com>
2025-10-05 12:50:53 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = -1;
+    while(n != 0){
+        cin >> n;
+        if(n != 0){
+            cout << n << "\n";
+        }
+    }
+    return 0;
+}
