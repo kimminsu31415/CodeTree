bbde7c3fa5030dd64f68d3bdb2bc2e9f9f01d40c
Minsu Kim <kimminsu31415@gmail.com>
2025-10-07 15:52:23 +0900

    Title: 좀 더 어려운 수학 점수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a_math, a_eng, b_math, b_eng;
+    cin >> a_math >> a_eng;
+    cin >> b_math >> b_eng;
+
+    if(a_math > b_math){
+        cout << 'A';
+  
+    }else if(b_math > a_math){
+        cout << 'B';
+    
+    }else{
+        if(a_eng > b_eng){
+            cout << 'A';
+      
+        }else if(b_eng > a_eng){
+            cout << 'B';
+        }
+    }
+    return 0;
+}
