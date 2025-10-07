c1daa5a4bb4493f562dd74b4341e05700aa2d9d8
Minsu Kim <kimminsu31415@gmail.com>
2025-10-07 15:59:40 +0900

    Title: 최댓값 구하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251007/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a > b && a > c){
+        cout << a;
+    }else if(b > c && b > a){
+        cout << b;
+    }else if(c > a && c > b){
+        cout << c;
+    }
+    return 0;
+}
