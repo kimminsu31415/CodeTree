1a9a4ab8e82fd838c74535ca5be646b44b0951a4
Minsu Kim <kimminsu31415@gmail.com>
2025-10-07 12:43:36 +0900

    Title: 실수와 문자 받아 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c;
+    double a, b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cin >> c >> a >> b;
+
+    cout << c << endl << a << endl << b;
+
+    return 0;
+}
