b7f8ea9e5d2f5cbcf5c6acc913622b68a84d8662
Minsu Kim <kimminsu31415@gmail.com>
2025-10-07 12:34:00 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c;
+    cout << fixed;
+    cout.precision(3);
+    cin >> a >> b >> c;
+
+    cout << a << endl << b << endl << c;
+    return 0;
+}
