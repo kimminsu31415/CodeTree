55cd2d8184137704c6b1d5b41e8ce59205cd2147
Minsu Kim <kimminsu31415@gmail.com>
2025-10-07 13:15:15 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int front, end;
+    cin >> front;
+    cin.get();
+    cin >> end;
+
+    cout << front << end;
+    return 0;
+}
