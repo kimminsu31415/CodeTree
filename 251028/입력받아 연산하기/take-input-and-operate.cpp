743256078eeac5716ea04ab4948c1408c20bb410
Minsu Kim <kimminsu31415@gmail.com>
2025-10-28 18:55:53 +0900

    Title: 입력받아 연산하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251028/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    cout << a+87 << endl;
+    cout << b % 10;
+    return 0;
+}
