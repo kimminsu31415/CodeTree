d1b6f2dc6ecd7b7863f616349256d7be2d608975
Minsu Kim <kimminsu31415@gmail.com>
2025-12-16 08:46:09 +0900

    Title: Top K 숫자 구하기, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251216/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_N 100
+
+int n, k;
+int arr[MAX_N];
+
+int main() {
+    cin >> n >> k;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr+n);
+
+    cout << arr[k-1];
+    
+    return 0;
+}
