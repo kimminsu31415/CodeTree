229d0c4659fff5a7c6681b0f3340c433774ed817
Minsu Kim <kimminsu31415@gmail.com>
2025-12-30 20:45:46 +0900

    Title: 수 등장 횟수, Time: 105ms, Memory: 2MB, Status: Passed - Codetree

--- 251230/수 등장 횟수/number-frequency.cpp
+++ 251230/수 등장 횟수/number-frequency.cpp
@@ -4,36 +4,25 @@ using namespace std;
 
 // 변수 설정
 int n, m;
-unordered_map<int, int> arr_to_index;
+unordered_map<int, int> num_count;
 
 int main() {
     // 입력
     cin >> n >> m;
 
-    // 배열 설정
-    int arr[n];
-
     // 배열 입력 & 해시맵 입력
     for(int i = 0; i < n; i++){
-        cin >> arr[i];
-        arr_to_index[i] = arr[i];
+        int temp;
+        cin >> temp;
+        num_count[temp]++;
     }
 
     for(int j = 0; j < m; j++){
         // 찾을 값 입력
-        int value, value_total;
+        int value;
         cin >> value;
 
-        // 해시값 찾기
-        for(int k = 0; k < n; k++){
-            if(arr_to_index[k] == value){
-                value_total += 1;
-            }
-        }
-
-        // 출력 & 초기화
-        cout << value_total << " ";
-        value_total = 0;
+        cout << num_count[value] << " ";
     }
 
     return 0;
