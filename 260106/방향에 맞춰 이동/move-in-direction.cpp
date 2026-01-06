2172641822b620d38779bcf39338bb91412eec9a
Minsu Kim <kimminsu31415@gmail.com>
2026-01-06 17:15:51 +0900

    Title: 방향에 맞춰 이동, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260106/방향에 맞춰 이동/move-in-direction.cpp
+++ 260106/방향에 맞춰 이동/move-in-direction.cpp
@@ -1,39 +1,31 @@
 #include <iostream>
 using namespace std;
 
+#define DIR_NUM 4
+
 int main() {
     // 변수 설정
     int n, x = 0, y = 0;
     // 서 동 북 남
-    int dx[] = {-1, 1, 0, 0};
-    int dy[] = {0, 0, 1, -1};
+    int dx[DIR_NUM] = {-1, 1, 0, 0};
+    int dy[DIR_NUM] = {0, 0, 1, -1};
 
     // 입력
     if(!(cin >> n)) return 0;
 
     for(int i = 0; i < n; i++){
         char go;
-        int distance;
-        cin >> go >> distance;
+        int dist;
+        cin >> go >> dist;
 
-            switch(go){
-        case 'W':
-            x += (distance * dx[0]);
-            y += (distance * dy[0]);
-            break;
-        case 'E':
-            x += (distance * dx[1]);
-            y += (distance * dy[1]);
-            break;
-        case 'N':
-            x += (distance * dx[2]);
-            y += (distance * dy[2]);
-            break;
-        case 'S':
-            x += (distance * dx[3]);
-            y += (distance * dy[3]);
-            break;
-    }
+        int dir;
+        if(go == 'W'){dir = 0;}
+        else if(go == 'E'){dir = 1;}
+        else if(go == 'N'){dir = 2;}
+        else if(go == 'S'){dir = 3;}
+
+        x += dx[dir] * dist;
+        y += dy[dir] * dist;
     }
 
 
